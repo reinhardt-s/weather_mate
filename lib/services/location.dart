cd62c2d3d89f23f741581d83487eeb5105415e27
Stefan Reinhardt <s_reinhardt@outlook.de>
2023-06-25 20:48:32 +0200

    Abschnitt 4 abgeschlossen

--- lib/services/location.dart
+++ lib/services/location.dart
@@ -4,7 +4,18 @@ class Location {
   double? latitude;
   double? longitude;
 
+  Future<void> checkPermission() async {
+    // Prüft, ob die App die Berechtigung hat, auf den Standort zuzugreifen
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    // Wenn die App keine Berechtigung hat, wird der Benutzer um Erlaubnis gebeten
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+  }
+
   Future<void> getCurrentLocation() async {
+    await checkPermission();
     try {
       Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
       latitude = position.latitude;
